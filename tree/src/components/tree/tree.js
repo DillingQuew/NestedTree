e4489f39cd00af6defa930d9c2808f261afaae82
Denis <daodawich@gmail.com>
2024-01-13 23:46:45 +0600

    done component for recursive render items

--- tree/src/components/tree/tree.js
+++ tree/src/components/tree/tree.js
@@ -1,13 +1,17 @@
-import {useState} from "react";
+import {useState, useMemo} from "react";
 
 export default function Tree({data}) {
-    return (
-        <ul>
-            {renderItem({data})}
-        </ul>
-    );
+    return ( <ul>{renderItem(data)}</ul>);
 }
 
-export function renderItem({data}) {
-    
+export function renderItem(data) {
+    return data.map((arr) => {
+        return (
+            <>
+                <li>{arr.title}</li>
+                {!!arr?.childs ? <ul>{ renderItem(arr.childs)}</ul> : null}
+
+            </>
+        )
+    })
 }
